985d40c335d8aa3cbb52e216a7534432c9ddaa64
vitames404 <vitorames@protonmail.com>
2024-03-12 13:14:54 +1300

    Small changes to the pictures

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Container,  Flex, Box, Grid, Heading, useColorModeValue } from "@chakra-ui/react"
+import { Container,  Flex, Box, Grid, Heading, useColorModeValue, Image } from "@chakra-ui/react"
 
 export default function Index() {
     return(
@@ -19,9 +19,33 @@ export default function Index() {
                 <Box display={{ md: 'flex' }}>
                     <Box flexGrow={1}>
                         <Heading as="h2" variant="page-title">
-                            Takuya Matsuyama
+                            Vitames404
                         </Heading>
                         <p>Digital Craftsman ( Artist / Developer / Designer )</p>
+                    </Box>
+                    <Box
+                    flexShrink={0}
+                    mt={{ base: 4, md: 0 }}
+                    ml={{ md: 6 }}
+                    textAlign="center"
+                    >
+                        <Box
+                        borderColor="whiteAlpha.800"
+                        borderWidth={2}
+                        borderStyle="solid"
+                        w="100px"
+                        h="100px"
+                        display="inline-block"
+                        borderRadius="full"
+                        overflow="hidden"
+                        >
+                            <Image
+                            src="pfp.jpg"
+                            alt="Profile image"
+                            width="100"
+                            height="100"
+                            />
+                        </Box>
                     </Box>
                 </Box>
             </Box>
